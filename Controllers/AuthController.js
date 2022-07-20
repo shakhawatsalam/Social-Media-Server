d755746efa914e2f4bcfef6b06a0f67987f5d1fc
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-07-20 10:00:14 +0600

    login is added

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -28,4 +28,20 @@ export const registerUser = async (req, res) => {
 
 //  loginUser
 
+export const loginUser = async (req, res) => {
+    const { username, password } = req.body;
 
+    try {
+        const user = await UserModel.findOne({ username: username });
+        if (user) {
+            const validity = await bcrypt.compare(password, user.password)
+
+            validity?res.status(200).json(user): res.status(400).json("Wrong Password")
+        }
+        else {
+            res.status(404).json("User does not exists");
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
