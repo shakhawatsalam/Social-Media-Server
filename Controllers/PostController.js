08933d3dd3b27d06020de80cdf15c80fe80fe630
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-07-22 11:56:36 +0600

    post delete Api done

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -44,3 +44,21 @@ export const updatePost = async (req, res) => {
 
 
 }
+
+//delete a post 
+export const deletePost = async (req, res) => {
+    const id = req.params.id;
+    const { userId } = req.body;
+
+    try {
+        const post = await PostModel.findById(id);
+        if (post.userId === userId) {
+            await post.deleteOne();
+            res.status(200).json("Post deleted successfully");
+        } else {
+            res.status(403).json("Action forbidden");
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
