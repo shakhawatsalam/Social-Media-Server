c9a5106dc8c846e8d12fc30898755d02ab4105f5
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-07-24 11:24:57 +0600

    like and disliked function is done

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -1,6 +1,7 @@
-import e from "express";
+import express from "express";
 import mongoose from "mongoose";
-import PostModel from "../Models/postModal.js";
+import PostModel from "../Models/postModal.js"
+import UserModel from "../Models/userModel.js";
 
 
 //Create new Post 
@@ -62,3 +63,57 @@ export const deletePost = async (req, res) => {
         res.status(500).json(error)
     }
 }
+
+
+// like/dislike a post
+export const likePost = async (req, res) => {
+    const id = req.params.id;
+    const { userId } = req.body;
+
+    try {
+        const post = await PostModel.findById(id);
+        if (!post.likes.includes(userId)) {
+
+            await post.updateOne({ $push: { likes: userId } })
+            res.status(200).json("Post Liked")
+        }
+        else {
+            await post.updateOne({ $pull: { likes: userId } })
+            res.status(200).json("Post unLiked")
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+//get timeline post
+export const getTimelinePost = async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        const currentUserPosts = await PostModel.find({ userId: userId });
+        const followingPosts = await UserModel.aggregate([
+            {
+                $match: {
+                    _id : new mongoose.Types.ObjectId(userId)
+                }
+            }, 
+            {
+                $lookup: {
+                    from: "posts", 
+                    localField: "following", 
+                    foreignField: "userId", 
+                    as: "followingPosts"
+                }
+            }, 
+            {
+                $project: {
+                    followingPosts: 1, 
+                    _id : 0
+                }
+            }
+        ])
+    } catch (error) {
+        res.status(500).json(error)
+    }
+} 
